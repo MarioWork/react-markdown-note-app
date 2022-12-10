d46219689fea19742e768854cab3faf444eda802
MarioWork <mariovieira.work@hotmail.com>
2022-12-10 19:39:15 -0100

    Updates

---
+++ precache-manifest.533fbcf0eb1a7fea3cdcd83d84e5e7f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfd5619ff97a025b7619",
+    "url": "/react-markdown-note-app/static/css/main.5ea1d0b2.chunk.css"
+  },
+  {
+    "revision": "cfd5619ff97a025b7619",
+    "url": "/react-markdown-note-app/static/js/main.cfd5619f.chunk.js"
+  },
+  {
+    "revision": "2d53d1313f8f51086fc0",
+    "url": "/react-markdown-note-app/static/css/1.cbd6f7d9.chunk.css"
+  },
+  {
+    "revision": "2d53d1313f8f51086fc0",
+    "url": "/react-markdown-note-app/static/js/1.2d53d131.chunk.js"
+  },
+  {
+    "revision": "87af5b17cc884236ef83",
+    "url": "/react-markdown-note-app/static/js/runtime~main.87af5b17.js"
+  },
+  {
+    "revision": "d1e42a3494f1172e57638f65f3b4b990",
+    "url": "/react-markdown-note-app/index.html"
+  }
+];
